8c28cb10302b3ff49c0231c5a9127491a706e0d8
cschallenberger <cleite@ionatec.com.br>
2017-11-28 19:29:20 -0200

    commit 2

---
+++ server-socket-read.js
@@ -0,0 +1,48 @@
+var fs = require("fs");
+var net = require('net');
+
+var getFileName = function(data){
+	var headers = data.split("\n");
+	var parts = headers[0].split(" ");
+	return parts[1];
+}
+
+var sendHeader = function(clientSocket, statusCode){
+	if(statusCode == 200){
+		clientSocket.write("HTTP/1.1 200 OK\r\n");
+	}else if (statusCode == 404){
+		clientSocket.write("HTTP/1.1 404 NOT FOUND\r\n");
+	}
+	clientSocket.write("Server: Talita/1.0.0 (WIN10)\r\n");
+	clientSocket.write("Connection: close\r\n");
+	clientSocket.write("\r\n");
+}
+
+var sendNotFound = function(clientSocket){
+	fs.readFile("./404.html", "utf8", function (err, data){
+		sendHeader(clientSocket, 404);
+		clientSocket.write(data);
+		clientSocket.destroy();
+	});
+}
+
+var server = net.createServer(function(clientSocket){
+    console.log('Client connected');
+
+    clientSocket.on('data', function(data){
+        //console.log('Client sent: ' + data.toString());
+		var fileName = getFileName(data.toString());
+		fs.readFile("." + fileName, function (err, fileData){
+			if (err){
+				sendNotFound(clientSocket);
+				return console.log(err); //error
+			}
+			sendHeader(clientSocket, 200);
+			clientSocket.write(fileData);
+			clientSocket.destroy();
+		});
+    });
+});
+
+server.listen(2001, '127.0.0.1');
+console.log('Subiu o server');
--- server-socket.js
+++ server-socket.js
@@ -1,19 +1,58 @@
 var net = require('net');
 var fs = require('fs');
 
-var server = net.createServer(function(socket){
-    console.log('Entrou');
-    socket.write('Estamos conectado!');
+var getFileName = function(data){
+  var headers = data.split("\n");
+  var recParts = headers[0].split(" ");
+  //console.log(headers);
+  //console.log("Request: " + recParts[1]);
+  return recParts[1];
+}
 
-    socket.on('end', function(){
-        console.log('Saiu');
+var sendHeader = function(socket, statusCode){
+  if(statusCode == 200){
+    socket.write("HTTP/1.1 200 OK\r\n");
+  }else if (statusCode == 404){
+    socket.write("HTTP/1.1 404 NOT FOUND\r\n");
+  }
+  socket.write("Server: Apache/1.3.27 (Unix)  (Red-Hat/Linux)\r\n");
+  socket.write("Last-Modified: Wed, 08 Jan 2003 23:11:55 GMT");
+  socket.write("Connection: close\r\n");
+  socket.write("Content-Type: text/html; charset=UTF-8\r\n");
+  socket.write("\r\n");
+}
+
+var sendNotFound = function(socket){
+  console.log("ERROUUUUU");
+  fs.readFile("404.html", "utf8", function (err, data){
+    console.log(data);
+  });
+  // fs.readFile("trabalho.html", "utf8", function (err, data){
+  //   sendHeader(socket, 200);
+  //   socket.write(data);
+  //   socket.destroy();
+}
+
+var server = net.createServer(function(socket){
+  console.log('Entrou');
+
+  socket.on('data', function(data){
+    var fileName = getFileName(data.toString());
+    fs.readFile("." + fileName, "utf8", function (err, fileData){
+      if(err){
+        console.log("eRRRRROuuuu");
+        sendNotFound(socket);
+        console.log(err);
+      }
+      sendHeader(socket, 200);
+      socket.write(fileData);
+      socket.destroy();
     });
-    socket.on('data', function(data){
-        console.log('Cliente disse: ' + data.toString());
-        fs.readFile("./sockets/trabalho.html", "utf8", function (err, data){
-          console.log(data);
-        });
-    });
+  });
+
+  socket.on('end', function(){
+    console.log('Saiu');
+  });
 });
 
 server.listen(2001, '127.0.0.1');
